049118628e9dd3ffe16e8b6ef449da92bce7a19d
assaf <asssaf@localhost>
2022-08-06 18:12:11 -0700

    add serial debug

--- attiny817-timer.ino
+++ attiny817-timer.ino
@@ -1,5 +1,8 @@
 #include <avr/sleep.h>
 
+#define DEBUG true
+#define Serial if(DEBUG)Serial
+
 const int LED_BUILTIN_817 = 5;
 const int EN_OUT_PIN = 19;
 const unsigned long ON_TIME_MILLIS = 5000;
@@ -18,9 +21,17 @@ void wdt_reset() {
 
 // the setup function runs once when you press reset or power the board
 void setup() {
+  Serial.begin(115200);
+
+  // set gpio pins to input to minimize power use
   for (int pin = 0; pin < 20; ++pin) {
+    if (DEBUG && (pin == 8) || (pin == 9)) {
+      // leave the rx/tx pins alone
+      continue;
+    }
     pinMode(pin, INPUT_PULLUP); 
   }
+
   // initialize digital pin LED_BUILTIN as an output.
   pinMode(LED_BUILTIN_817, OUTPUT);
   pinMode(EN_OUT_PIN, OUTPUT);
@@ -31,7 +42,8 @@ void setup() {
 // the loop function runs over and over again forever
 void loop() {
   wdt_enable();
-  unsigned long StartTime = millis();
+  unsigned long start_time = millis();
+  Serial.println("started");
   
   digitalWrite(LED_BUILTIN_817, LOW);     // turn the LED on 
   digitalWrite(EN_OUT_PIN, HIGH);         // set enable pin high
@@ -39,6 +51,9 @@ void loop() {
 
   digitalWrite(EN_OUT_PIN, LOW);          // set enable pin low
   digitalWrite(LED_BUILTIN_817, HIGH);    // turn the LED off
+
+  Serial.println("going to sleep");
+  Serial.flush();
   wdt_reset();
   sleep_cpu();
 }
